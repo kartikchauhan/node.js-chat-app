cf6d851a1b13566bf4fa8ae793089d0fdfae7f2b
kartik <chauhan.kartik25@gmail.com>
2018-08-06 02:09:34 +0530

    broadcasting messaged to all users done

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const io = socket(server);
 
 io.on('connection', function(socket) {
     console.log('new socket connection setup successfully with socket id ', socket.id);
+
     socket.on('chat', function(data) {
         io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', function(data) {
+        socket.broadcast.emit('typing', data);
     });
 });
--- public/chat.js
+++ public/chat.js
@@ -3,15 +3,27 @@ const socket = io.connect('http://localhost:8080');
 var message = document.getElementById('message'),
     handle = document.getElementById('handle'),
     sendButton = document.getElementById('send'),
-    output = document.getElementById('output');
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
 
-sendButton.addEventListener('click', function() {
+sendButton.addEventListener('click', function() {    
     socket.emit('chat', {
         handle: handle.value,
         message: message.value
     });
+    console.log('clearing the message');
+    message.value = '';
+});
+
+message.addEventListener('keypress', function() {
+    socket.emit('typing', handle.value);
 });
 
 socket.on('chat', function(data) {
-    output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.handle + ':</strong> ' + data.message + '</p>';
+});
+
+socket.on('typing', function(data) {
+    feedback.innerHTML = '<p><em>' + data + ' is typing...</em></p>';
 });
