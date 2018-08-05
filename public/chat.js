cb9c3723f6409bf4ce4474ee2c1f0b07339bcb1c
kartik <chauhan.kartik25@gmail.com>
2018-08-06 02:42:02 +0530

    url changed for client side sockets

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://localhost:8080');
+const socket = io.connect('https://node-chat-app-sockets.herokuapp.com:8080');
 
 var message = document.getElementById('message'),
     handle = document.getElementById('handle'),
