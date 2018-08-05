13e38f9d63de85a604cb98eb51a04b7d09e22fa3
kartik <chauhan.kartik25@gmail.com>
2018-08-06 00:40:25 +0530

    socket.io setup on server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+
+const server = app.listen(8080, function() {
+    console.log('Server started successfully');
+});
+
+app.use(express.static('public'));
+
+const io = socket(server);
+
+io.on('connection', function(socket) {
+    console.log('new socket connection setup successfully');
+});
